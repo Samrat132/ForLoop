c1574252c01ee7a5f05c9927c1beea21d89f0119
Samrat132 <84699138+Samrat132@users.noreply.github.com>
2021-06-10 20:07:25 +0545

    area

--- area.py
+++ area.py
@@ -1,6 +1,6 @@
 #Write a program to print area of rectangle three times by taking user input?
 for i in range(3):
-width=int(input('Enter the value of width'))
-height=int(input('Enter the value of height'))
-area=width*height
-print(area)
+    length=int(input('Enter the value of length'))
+    breadth=int(input('Enter the value of breadth'))
+    area=length*breadth
+    print(area)
